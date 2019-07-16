324c5c769ea4cf9625e2f29a96c2746d5ac2ded6
Evgeniy Chebanov <evenchik86@gmail.com>
2019-07-16 18:43:30 +0300

    Завершение функционала изменения стилей изображения на основе слайдера, рефакторинг

--- js/main.js
+++ js/main.js
@@ -18,10 +18,48 @@ var NameTemplates = {
 
 var ESC_KEYCODE = 27;
 var EFFECT_CLASSNAME_PREFIX = 'effects__preview--';
-var EFFECT_NONE_VALUE = 'none';
-var EFFECT_NONE_CLASS = EFFECT_CLASSNAME_PREFIX + EFFECT_NONE_VALUE;
+var EFFECT_NONE_NAME = 'none';
+var EFFECT_NONE_CLASS = EFFECT_CLASSNAME_PREFIX + EFFECT_NONE_NAME;
 var SCALE_STEP = 25;
 var SLIDER_DEFAULT_VALUE = 100;
+
+var EFFECTS_INFO = [
+  {
+    type: 'chrome',
+    filter: 'grayscale',
+    minValue: 0,
+    maxValue: 1,
+    unit: ''
+  },
+  {
+    type: 'sepia',
+    filter: 'sepia',
+    minValue: 0,
+    maxValue: 1,
+    unit: ''
+  },
+  {
+    type: 'marvin',
+    filter: 'invert',
+    minValue: 0,
+    maxValue: 100,
+    unit: '%'
+  },
+  {
+    type: 'phobos',
+    filter: 'blur',
+    minValue: 0,
+    maxValue: 3,
+    unit: 'px'
+  },
+  {
+    type: 'heat',
+    filter: 'brightness',
+    minValue: 1,
+    maxValue: 3,
+    unit: ''
+  }
+];
 
 // Генерация моков и добавление фотографий на страницу
 
@@ -112,58 +150,111 @@ var pictures = generateMocks();
 addPicturesToPage(pictures);
 
 var uploadButton = document.querySelector('#upload-file');
-uploadButton.addEventListener('change', function () {
-  openPopup();
+uploadButton.addEventListener('change', openPopup);
+
+
+// Компоненты окна загрузки загрузки изображения
+var uploadedImageForm = document.querySelector('.img-upload__overlay');
+var uploadedImage = uploadedImageForm.querySelector('.img-upload__preview');
+var imageEffectsList = document.querySelectorAll('.effects__list .effects__radio');
+var cancelButton = uploadedImageForm.querySelector('#upload-cancel');
+var effectLevelControls = uploadedImageForm.querySelector('.img-upload__effect-level');
+var descriptionField = document.querySelector('.text__description');
+
+// Компоненты слайдера
+var sliderPin = effectLevelControls.querySelector('.effect-level__pin');
+var sliderLineDepth = effectLevelControls.querySelector('.effect-level__depth');
+var sliderValueControl = effectLevelControls.querySelector('.effect-level__value');
+
+// Компоненты масштабирования изображения
+var scaleControls = uploadedImageForm.querySelector('.img-upload__scale');
+var scaleControlSmaller = scaleControls.querySelector('.scale__control--smaller');
+var scaleControlBigger = scaleControls.querySelector('.scale__control--bigger');
+var scaleControlValue = scaleControls.querySelector('.scale__control--value');
+
+// Наименование текущего выбранного эффекта
+var currentEffectName = EFFECT_NONE_NAME;
+
+
+// Масштабирование изображения
+function changeScale(changeStep) {
+  var currentValue = parseInt(scaleControlValue.value, 10);
+  var newValue = currentValue + changeStep;
+  scaleControlValue.value = newValue + '%';
+  uploadedImage.style.transform = 'scale(' + newValue / 100 + ')';
+}
+
+scaleControlSmaller.addEventListener('click', function () {
+  if (parseInt(scaleControlValue.value, 10) > 25) {
+    changeScale(-SCALE_STEP);
+  }
+});
+scaleControlBigger.addEventListener('click', function () {
+  if (parseInt(scaleControlValue.value, 10) < 100) {
+    changeScale(SCALE_STEP);
+  }
 });
 
+// Управлени эффектами
+function setSliderPositionByValue(value) {
+  var valueInPixels = Math.floor(sliderLineDepth.offsetParent.clientWidth * value / 100);
+  sliderPin.style.left = valueInPixels + 'px';
+  sliderLineDepth.style.width = valueInPixels + 'px';
+}
+
+function removeImageFilters(image) {
+  for (var j = 0; j < image.classList.length; j++) {
+    if (image.classList[j].startsWith(EFFECT_CLASSNAME_PREFIX)) {
+      image.classList.remove(image.classList[j]);
+    }
+  }
+}
+
+function setImageStyle(effectType, value) {
+  for (var j = 0; j < EFFECTS_INFO.length; j++) {
+    var currentEffect = EFFECTS_INFO[j];
+    if (currentEffect.type === effectType) {
+      var styleValue = currentEffect.maxValue * +value / 100;
+      if (styleValue < currentEffect.minValue) {
+        styleValue = currentEffect.minValue;
+      }
+      var styleString = currentEffect.filter + '(' + styleValue + currentEffect.unit + ')';
+      uploadedImage.style.filter = styleString;
+      return;
+    }
+  }
+  uploadedImage.style.filter = '';
+}
+
+function addEffectClickHandler(effect, image) {
+  effect.addEventListener('click', function (evt) {
+    removeImageFilters(image);
+    image.classList.add(EFFECT_CLASSNAME_PREFIX + effect.value);
+    if (effect.value === EFFECT_NONE_NAME) {
+      effectLevelControls.classList.add('hidden');
+    } else {
+      effectLevelControls.classList.remove('hidden');
+      sliderValueControl.value = SLIDER_DEFAULT_VALUE;
+      setSliderPositionByValue(SLIDER_DEFAULT_VALUE);
+    }
+    currentEffectName = evt.target.value;
+    setImageStyle(currentEffectName, SLIDER_DEFAULT_VALUE);
+  });
+}
+
+for (var i = 0; i < imageEffectsList.length; i++) {
+  addEffectClickHandler(imageEffectsList[i], uploadedImage);
+}
+
+
+// Набор действий при открытии попапа
 function openPopup() {
-  // Компоненты окна загрузки загрузки изображения
-  var uploadedImageForm = document.querySelector('.img-upload__overlay');
-  var uploadedImage = uploadedImageForm.querySelector('.img-upload__preview');
-  var imageEffectsList = document.querySelectorAll('.effects__list .effects__radio');
-  var cancelButton = uploadedImageForm.querySelector('#upload-cancel');
-  var effectLevelControls = uploadedImageForm.querySelector('.img-upload__effect-level');
-  var descriptionField = document.querySelector('.text__description');
-
-  // Компоненты слайдера
-  var sliderPin = effectLevelControls.querySelector('.effect-level__pin');
-  var sliderLineDepth = effectLevelControls.querySelector('.effect-level__depth');
-  var sliderValueControl = effectLevelControls.querySelector('.effect-level__value');
-
-  // Компоненты масштабирования изображения
-  var scaleControls = uploadedImageForm.querySelector('.img-upload__scale');
-  var scaleControlSmaller = scaleControls.querySelector('.scale__control--smaller');
-  var scaleControlBigger = scaleControls.querySelector('.scale__control--bigger');
-  var scaleControlValue = scaleControls.querySelector('.scale__control--value');
-
-
-  // Набор действий при открытии попапа
   uploadedImageForm.classList.remove('hidden');
   setImageFormDefaultState();
 
   document.addEventListener('keydown', onPopupEscPress);
   cancelButton.addEventListener('click', function () {
     closePopup();
-  });
-
-  for (var i = 0; i < imageEffectsList.length; i++) {
-    addEffectClickHandler(imageEffectsList[i], uploadedImage);
-  }
-
-  // TODO Это событие не будет срабатывать!
-  sliderValueControl.addEventListener('change', function () {
-    alert('sdfsdsdf!');
-  });
-
-  scaleControlSmaller.addEventListener('click', function () {
-    if (parseInt(scaleControlValue.value, 10) > 25) {
-      changeScale(-SCALE_STEP);
-    }
-  });
-  scaleControlBigger.addEventListener('click', function () {
-    if (parseInt(scaleControlValue.value, 10) < 100) {
-      changeScale(SCALE_STEP);
-    }
   });
 
   descriptionField.addEventListener('focusin', function () {
@@ -174,22 +265,14 @@ function openPopup() {
   });
 
 
-  // Функции при открытии попапа
   function setImageFormDefaultState() {
     removeImageFilters(uploadedImage);
-    setEffectButtonChecked(imageEffectsList, EFFECT_NONE_VALUE);
+    setEffectButtonChecked(imageEffectsList, EFFECT_NONE_NAME);
     uploadedImage.classList.add(EFFECT_NONE_CLASS);
     uploadedImage.style.transform = '';
+    uploadedImage.style.filter = '';
     scaleControlValue.value = '100%';
     effectLevelControls.classList.add('hidden');
-  }
-
-  function removeImageFilters(image) {
-    for (var j = 0; j < image.classList.length; j++) {
-      if (image.classList[j].startsWith(EFFECT_CLASSNAME_PREFIX)) {
-        image.classList.remove(image.classList[j]);
-      }
-    }
   }
 
   function setEffectButtonChecked(effectsList, effectValue) {
@@ -212,91 +295,55 @@ function openPopup() {
     document.removeEventListener('keydown', onPopupEscPress);
     uploadButton.value = '';
   }
+}
 
-  function setImageEffectByValue(value) {
-    // TODO менять стили
-    console.log('setImageEffectStyle');
-  }
 
-  function setSliderPositionByValue(value) {
-    var valueInPixels = Math.floor(sliderLineDepth.offsetParent.clientWidth * value / 100);
-    sliderPin.style.left = valueInPixels + 'px';
-    sliderLineDepth.style.width = valueInPixels + 'px';
-  }
+// Управление перемещением слайдера
+sliderPin.addEventListener('mousedown', function (evt) {
+  var sliderLinePosition = sliderPin.offsetParent.getBoundingClientRect();
+  var sliderPinPosition = sliderPin.getBoundingClientRect();
 
-  function addEffectClickHandler(effect, image) {
-    effect.addEventListener('click', function () {
-      removeImageFilters(image);
-      image.classList.add(EFFECT_CLASSNAME_PREFIX + effect.value);
-      if (effect.value === EFFECT_NONE_VALUE) {
-        effectLevelControls.classList.add('hidden');
-      } else {
-        effectLevelControls.classList.remove('hidden');
-        sliderValueControl.value = SLIDER_DEFAULT_VALUE;
-        setSliderPositionByValue(SLIDER_DEFAULT_VALUE);
-        setImageEffectByValue(SLIDER_DEFAULT_VALUE);
-      }
-    });
-  }
-
-  function changeScale(changeStep) {
-    var currentValue = parseInt(scaleControlValue.value, 10);
-    var newValue = currentValue + changeStep;
-    scaleControlValue.value = newValue + '%';
-    uploadedImage.style.transform = 'scale(' + newValue / 100 + ')';
-  }
-
-  function setEffectLevel() {
+  function setSliderValue() {
     var pinPosition = sliderPin.offsetLeft;
     var sliderValue = Math.floor(pinPosition / sliderLineDepth.offsetParent.clientWidth * 100);
-    // Приведение к int, т.к. input type="number" возвращает value с типом string
+    // Приведение к int, т.к. input всегда возвращает value с типом string
     if (+sliderValueControl.value !== sliderValue) {
       sliderValueControl.value = sliderValue;
     }
   }
 
-  // Управление перемещением слайдера
-  sliderPin.addEventListener('mousedown', function (evt) {
-    var sliderLinePosition = sliderPin.offsetParent.getBoundingClientRect();
-    var sliderPinPosition = sliderPin.getBoundingClientRect();
+  // Корректировка координаты с учетом наличия у пина размеров
+  function getSliderPinCorrectionX(pinPosition, currentX) {
+    var pinCenterX = pinPosition.left + pinPosition.width / 2;
+    var pinCorrectionX = pinCenterX - currentX;
+    return pinCorrectionX;
+  }
 
-    // Корректировка координаты с учетом наличия у пина размеров
-    function getSliderPinCorrectionX(pinPosition, currentX) {
-      var pinCenterX = pinPosition.left + pinPosition.width / 2;
-      var pinCorrectionX = currentX - pinCenterX;
-      return pinCorrectionX;
+  var pinCorrectionX = getSliderPinCorrectionX(sliderPinPosition, evt.clientX);
+  var startPinPositionX = evt.clientX + pinCorrectionX;
+
+  function onMouseMove(moveEvt) {
+    var newPinPositionX = moveEvt.clientX + pinCorrectionX;
+    if (newPinPositionX > sliderLinePosition.right) {
+      newPinPositionX = sliderLinePosition.right;
+    } else if (newPinPositionX < sliderLinePosition.left) {
+      newPinPositionX = sliderLinePosition.left;
     }
+    var shift = newPinPositionX - startPinPositionX;
+    startPinPositionX = newPinPositionX;
 
-    var startPositionX = evt.clientX;
-    var pinCorrectionX = getSliderPinCorrectionX(sliderPinPosition, evt.clientX);
+    sliderPin.style.left = (sliderPin.offsetLeft + shift) + 'px';
+    sliderLineDepth.style.width = (sliderLineDepth.offsetWidth + shift) + 'px';
+    setSliderValue();
+    setImageStyle(currentEffectName, sliderValueControl.value);
+  }
 
-    function onMouseMove(moveEvt) {
-      var newPositionX = moveEvt.clientX;
-      if (newPositionX > sliderLinePosition.right + pinCorrectionX) {
-        newPositionX = sliderLinePosition.right + pinCorrectionX;
-      } else if (newPositionX < sliderLinePosition.left + pinCorrectionX) {
-        newPositionX = sliderLinePosition.left + pinCorrectionX;
-      }
-      var shift = newPositionX - startPositionX;
-      startPositionX = newPositionX;
-
-      sliderPin.style.left = (sliderPin.offsetLeft + shift) + 'px';
-      sliderLineDepth.style.width = (sliderLineDepth.offsetWidth + shift) + 'px';
-
-      // TODO заполнить value контрола, посчитав %
-      // Использовать setSliderPositionByValue?
-    }
-
-    function onMouseUp() {
-      setEffectLevel();
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    }
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-
-  // sliderPin.addEventListener('mouseup', function () {
-  //   setEffectLevel();
-  // });
-}
+  function onMouseUp() {
+    setSliderValue();
+    setImageStyle(currentEffectName, sliderValueControl.value);
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  }
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
